c2da1eb958d0f0a1aab7cf044c5ca8aa994c3892
le5ejumeau <le5ejumeau@gmail.com>
2013-09-20 10:01:24 -0700

    Ajout HAL.Test

---
+++ HAL.Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HAL.Library;
+using HAL.Library.Voice;
+
+namespace HAL.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VoiceSynthesis v = new VoiceSynthesis();
+
+            foreach (var item in v.GetVoices())
+            {
+
+            }
+
+            v.Speak("bonjour ceci est un test");
+        }
+    }
+}
